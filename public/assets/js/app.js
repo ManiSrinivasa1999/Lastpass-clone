503c76ce0ab4aa95f8077171bd5e801747c56c78
manisrinivasa <manisrinivasa1999@gmail.com>
2020-04-24 17:16:05 +0530

    updated the user signin options google,email,modile number.

---
+++ public/assets/js/app.js
@@ -0,0 +1,43 @@
+import {firebaseConfig} from './config.js';
+(
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      // const model = {
+
+      // };
+      const view = {
+        init: () => {
+          view.render();
+        },
+        render: () => {
+
+        },
+      };
+      const controller = {
+        init: () => {
+          view.init();
+        },
+      };
+      firebase.initializeApp(firebaseConfig);
+      const ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start('#firebaseui-auth-container', {
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          {
+            provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            defaultCountry: 'IN',
+          },
+        ],
+        signInSuccessUrl: 'user.html',
+      });
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          controller.init();
+        } else {
+          // no user signed in
+        }
+      });
+    }
+  }
+)();
---
+++ public/assets/js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCf2tK8OJqQTOLNfkzfFDOXnnZNYs-hbZQ',
+  authDomain: 'lastpass-e39d8.firebaseapp.com',
+  databaseURL: 'https://lastpass-e39d8.firebaseio.com',
+  projectId: 'lastpass-e39d8',
+  storageBucket: 'lastpass-e39d8.appspot.com',
+  messagingSenderId: '623457979411',
+  appId: '1:623457979411:web:832fde345bafa043dc43e6',
+};
