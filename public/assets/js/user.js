e3b9f7ae667482ddd100db2261465d8dc6352985
manisrinivasa <manisrinivasa1999@gmail.com>
2020-04-26 17:47:05 +0530

    updated with the user data store in firebase funtionality

---
+++ public/assets/js/user.js
@@ -0,0 +1,207 @@
+import {firebaseConfig} from './config.js';
+(
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      const profilePicture = document.getElementById('profile-picture');
+      const userName = document.getElementById('user-name');
+      const emailId = document.getElementById('email-id');
+      const settingsMenu = document.getElementById('settings-menu');
+      const logout = document.getElementById('logout');
+      const addPassword = document.getElementById('add-password');
+      const passwordContent = document.getElementById('password-content');
+      const closeModal = document.getElementById('close-modal');
+      const urlInput = document.getElementById('url-input');
+      const nameInput = document.getElementById('name-input');
+      const passwordInput = document.getElementById('password-input');
+      const userNameInput = document.getElementById('username-input');
+      const notesInput = document.getElementById('notes-input');
+      const savePassword = document.getElementById('save-password');
+      const model = {
+        user: { },
+        password: {
+          url: '',
+          name: '',
+          username: '',
+          sitePassword: '',
+          notes: '',
+        },
+        passwords: [],
+      };
+      const view = {
+        init: () => {
+          savePassword.onclick = () => {
+            controller.savePasswords();
+          },
+          urlInput.oninput = () => {
+            controller.handleUrlInput(urlInput.value);
+          },
+          nameInput.oninput = () => {
+            controller.handleNameInput(nameInput.value);
+          },
+          userNameInput.oninput = () => {
+            controller.handleUserNameInput(userNameInput.value);
+          },
+          passwordInput.oninput = () => {
+            controller.handlePasswordInput(passwordInput.value);
+          },
+          notesInput.oninput = () => {
+            controller.handleNotesInput(notesInput.value);
+          },
+          addPassword.onclick = () => {
+            passwordContent.classList.add('is-active');
+          },
+          closeModal.onclick = () => {
+            passwordContent.classList.remove('is-active');
+          },
+          settingsMenu.onclick = () => {
+            settingsMenu.classList.toggle('is-active');
+          },
+          logout.onclick = () => {
+            controller.logout();
+          },
+          view.render();
+        },
+        render: () => {
+          profilePicture.src = model.user.photoURL;
+          userName.innerHTML = model.user.displayName;
+          emailId.innerHTML = model.user.email;
+        },
+      };
+      const controller = {
+        handleNotesInput: (name) => {
+          if (controller.validateNotes(name)) {
+            notesInput.classList.add('is-success');
+            notesInput.classList.remove('is-danger');
+          } else {
+            notesInput.classList.remove('is-success');
+            notesInput.classList.add('is-danger');
+          }
+        },
+        handleUserNameInput: (name) => {
+          if (controller.validateUserName(name)) {
+            userNameInput.classList.add('is-success');
+            userNameInput.classList.remove('is-danger');
+          } else {
+            userNameInput.classList.remove('is-success');
+            userNameInput.classList.add('is-danger');
+          }
+        },
+        handlePasswordInput: (name) => {
+          if (controller.validatePassword(name)) {
+            passwordInput.classList.add('is-success');
+            passwordInput.classList.remove('is-danger');
+          } else {
+            passwordInput.classList.remove('is-success');
+            passwordInput.classList.add('is-danger');
+          }
+        },
+        handleNameInput: (name) => {
+          if (controller.validateName(name)) {
+            nameInput.classList.remove('is-danger');
+            nameInput.classList.add('is-success');
+          } else {
+            nameInput.classList.remove('is-success');
+            nameInput.classList.add('is-danger');
+          }
+        },
+        handleUrlInput: (name) => {
+          if (controller.validateUrl(name)) {
+            urlInput.classList.add('is-success');
+            urlInput.classList.remove('is-danger');
+          } else {
+            urlInput.classList.remove('is-success');
+            urlInput.classList.add('is-danger');
+          }
+        },
+        validateNotes: (name) => {
+          return name.length > 100;
+        },
+        validatePassword: (name) => {
+          return name.length > 5;
+        },
+        validateUserName: (name) => {
+          return name.length > 6;
+        },
+        validateName: (name) => {
+          return name.length > 5;
+        },
+        validateUrl: (name) => {
+          return name.length > 10;
+        },
+        validateAll: () => {
+          return controller.validateUrl(urlInput.value) &&
+          controller.validateName(nameInput.value) &&
+          controller.validateUserName(userNameInput.value) &&
+          controller.validatePassword(passwordInput.value) &&
+          controller.validateNotes(notesInput.value);
+        },
+        savePasswords: () => {
+          if (controller.validateAll()) {
+            db.collection('storePassword').add({
+              url: urlInput.value,
+              name: nameInput.value,
+              username: userNameInput.value,
+              sitePassword: passwordInput.value,
+              notes: notesInput.value,
+              time: new Date(),
+            })
+                .then(() => {
+                })
+                .catch((error) => {
+                  alert('please try later');
+                });
+          } else {
+            alert('Fill all areas');
+          }
+          urlInput.value = '';
+          nameInput.value = '';
+          userNameInput.value = '';
+          passwordInput.value = '';
+          notesInput.value = '';
+        },
+        logout: () => {
+          firebase.auth().signOut();
+        },
+        setUser: (user) => {
+          model.user = user;
+          view.render();
+        },
+        setPassword: (passwords) => {
+          model.passwords = passwords;
+          view.render();
+        },
+        init: () => {
+          db.collection('storePassword').onSnapshot(
+              (docSnapshot) => {
+                const passwords = [];
+                docSnapshot.forEach((doc) => {
+                  if (doc.exists) {
+                    const password = doc.data();
+                    password.id = doc.id;
+                    passwords.push(password);
+                  }
+                });
+                passwords.sort((task1, task2)=>{
+                  return task1.time.seconds-task2.time.seconds;
+                });
+                controller.setPassword(passwords);
+              },
+              (err) => {
+                alert(err);
+              });
+          view.init();
+        },
+      };
+      firebase.initializeApp(firebaseConfig);
+      const db = firebase.firestore();
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          controller.init();
+          controller.setUser(user);
+        } else {
+          window.location.href = './index.html';
+        }
+      });
+    }
+  }
+)();
